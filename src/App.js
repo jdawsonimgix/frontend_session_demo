1d3ac561d269f788d0bd4aa3215f5f46aed736a1
jdawsonimgix <jdawson@imgix.com>
2022-12-22 12:40:11 -0800

    updated all frontend code. Removed old code and comments

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import "./App.css";
 
 function App() {
   const [pic, setPic] = useState();
-  const [sessionData, setSessionData] = useState("no session data");
-  const [sessionSourceId, setSessionSourceId] = useState("no session"); //Used to check status.
+  const [sessionSourceId, setSessionSourceId] = useState(
+    "no session. Please choose a file"
+  ); //Used to check status.
   const [sessionStatus, setSessionStatus] = useState("No Status");
-  const [theContentType, setContentType] = useState("");
   const [sessionPresignedUrl, setSessionPresignedUrl] =
     useState("No session URL"); //Long amazon url
-  const [heldFormData, setHeldFormData] = useState("");
-  useEffect(() => {}, [sessionData, heldFormData]);
+  useEffect(() => {}, []);
 
   //IMGIX EXAMPLES: STARTING SESSION
   const imgixHandleSubmitForSessionStarting = async (e) => {
@@ -21,7 +20,10 @@ function App() {
     //"https://backend-sessions-demo.vercel.app/startImgixSession",
 
     const retrievedBackendData = await axios
-      .post("http://localhost:5001/startImgixSession", formData)
+      .post(
+        "https://backend-sessions-demo.vercel.app/startImgixSession",
+        formData
+      )
       .then(console.log("starting imgix session"))
       .catch((error) => console.log(error.message));
 
@@ -29,30 +31,14 @@ function App() {
 
     console.log(retrievedBackendData.data);
 
-    setSessionSourceId(retrievedBackendData.data.sessionIdBackend); //Stores session Source id
-    setSessionStatus(retrievedBackendData.data.sessionStatusBackend); //Session status
+    setSessionSourceId(retrievedBackendData.data.sessionIdBackend);
+    setSessionStatus(retrievedBackendData.data.sessionStatusBackend);
     setSessionPresignedUrl(
       retrievedBackendData.data.sessionPresignedUrlBackend
-    ); //Session Presigned url
-    //setHeldFormData(test.data.theBufferReturned.data); //Buffer
-    // setContentType(test.data.theFileType); //content-type
+    );
   };
   const imgixHandleChangeForSessionStarting = (e) => {
     setPic(e.target.files[0]);
-  };
-
-  //IMGIX EXAMPLE: PUT REQUEST WITH PRESIGNED SESSION URL
-  const sendFormDataPostRequest = async (e) => {
-    e.preventDefault();
-    const formDataPostRequest = new FormData();
-    formDataPostRequest.append("pic", pic);
-    formDataPostRequest.append("awsURL", sessionPresignedUrl);
-
-    //Starting session
-    const test = await axios
-      .post("http://localhost:5001/postSession", formDataPostRequest)
-      .then(console.log(""))
-      .catch((error) => console.log(error.message));
   };
 
   //IMGIX EXAMPLE: CHECK SESSION STATUS
@@ -62,7 +48,10 @@ function App() {
 
     //check session
     const sessionStatusForAxios = await axios
-      .post("http://localhost:5001/checkImgixSessionStatus", value)
+      .post(
+        "https://backend-sessions-demo.vercel.app/checkImgixSessionStatus",
+        value
+      )
       .then(console.log("Check imgix session"))
       .catch((error) => console.log(error.message));
 
@@ -78,7 +67,10 @@ function App() {
 
     //close session
     const sessionStatusForAxios = await axios
-      .post("http://localhost:5001/checkImgixCloseSession", valueData)
+      .post(
+        "https://backend-sessions-demo.vercel.app/checkImgixCloseSession",
+        valueData
+      )
       .then(console.log("Client - CLOSE imgix session"))
       .catch((error) => console.log(error.message));
 
@@ -87,29 +79,18 @@ function App() {
     );
   };
 
-  //See formData
-  const seeFormData = () => {
-    console.log(JSON.stringify(heldFormData));
-  };
-
   return (
     <div className='app'>
       <form className='form' onSubmit={imgixHandleSubmitForSessionStarting}>
         <input type='file' onChange={imgixHandleChangeForSessionStarting} />
         <br />
         <button>Starting a session</button>
-        <br />
-        <button onClick={sendFormDataPostRequest}>
-          Sending formData to Post Request
-        </button>
       </form>
-      <button onClick={imgixHandleCheckStatus}>Check session status</button>
-      <br />
       <button onClick={imgixHandleCloseSession}>Close Session</button>
       <br />
-      <button onClick={seeFormData}>See form data in Console log</button>
-      <br />
+      <button onClick={imgixHandleCheckStatus}>Check session status</button>
 
+      <br />
       <h3>The sessionSourceId is: {sessionSourceId}</h3>
       <h3>The sessionStatus is: {sessionStatus}</h3>
       <h3>The sessionPresignedUrl is: {sessionPresignedUrl}</h3>
